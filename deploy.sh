b3e8046d7c03227ca5ba11c457bb2bc3bfaa0dae
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-17 10:40:45 -0500

    psl

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,3 @@
-eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .travis/deploy_key.pem # this key should have push access
-ssh-add .travis/deploy_key.pem
 IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
