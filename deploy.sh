849135eda02661a868de48e6dce297b794d7b3ad
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-17 12:12:59 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 $DEPLOYMENT_KEY # this key should have push access
+ssh-add $DEPLOYMENT_KEY
 IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
-git remote add deploy ssh://node@$IP/cahbot/cahbot.git
+git remote add deploy ssh://jcai@$IP/cahbot/cahbot.git
 git push deploy
