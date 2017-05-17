0b9a0cf32bead69ce1edc83806b8b37229151978
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-17 12:29:48 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 eval "$(ssh-agent -s)" #start the ssh agent
-echo $DEPLOYMENT_KEY >> .travis/deployment_key.pub
-chmod 600 .travis/deployment_key.pub # this key should have push access
-ssh-add .travis/deployment_key.pub
+cat deployment_key.pub
+echo $DEPLOYMENT_KEY >> deployment_key.pub
+chmod 600 deployment_key.pub # this key should have push access
+ssh-add deployment_key.pub
+rm deployment_key.pub
 IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
