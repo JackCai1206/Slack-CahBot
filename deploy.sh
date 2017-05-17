46b136f65da8474cf74a255a4bed108f3803a2fb
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-17 15:42:21 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 eval "$(ssh-agent -s)" #start the ssh agent
 echo $DEPLOYMENT_KEY > deployment_key.pub
+echo $PASSPHRASE > passphrase
 chmod 600 deployment_key.pub # this key should have push access
-ssh-add deployment_key.pub
+ssh-add deployment_key.pub -p passphrase
 rm deployment_key.pub
 IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
