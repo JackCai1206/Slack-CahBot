85bffeb4ed559f46f234cf002e2a0ec9ebe1c28e
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-17 12:17:45 -0500

    pls

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 $DEPLOYMENT_KEY # this key should have push access
-ssh-add $DEPLOYMENT_KEY
+chmod 600 "$DEPLOYMENT_KEY" # this key should have push access
+ssh-add "$DEPLOYMENT_KEY"
 IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
