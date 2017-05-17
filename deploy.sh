c0122bcb6d260bba4d4095619c377b532e2dfa75
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-17 10:54:51 -0500

    try another user

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
-git remote add deploy ssh://jcai@45.56.70.141/cahbot/cahbot.git
+git remote add deploy ssh://node@$IP/cahbot/cahbot.git
 git push deploy
