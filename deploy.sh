b45d83adacfd2c797f25ef343fc8a5da1ca24f1e
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-24 11:47:01 -0500

    whios

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ eval "$(ssh-agent -s)" #start the ssh agent
 echo $DEPLOYMENT_KEY > deployment_key
 chmod 600 deployment_key # this key should have push access
 # ssh-add deployment_key
-rm deployment_key
+# rm deployment_key
 IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
