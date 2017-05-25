da21c9ae6fe3e6a54806b6c364ea1c38d1a5f4d1
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-25 14:29:26 -0500

    test

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 eval "$(ssh-agent -s)" #start the ssh agent
 echo $DEPLOYMENT_KEY > deployment_key
 chmod 600 deployment_key # this key should have push access
-# ssh-add deployment_key
-# rm deployment_key
+ssh-add deployment_key
+rm deployment_key
 IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
-echo "exec cat" > ap-cat.sh
-chmod a+x ap-cat.sh
-export DISPLAY=1
-echo $PASSPHRASE | SSH_ASKPASS=./ap-cat.sh ssh-add deployment_key
-rm ap-cat.sh
-git remote add deploy https://linode.michaelgira.me/slack-cah-bot/Slack-CahBot
+# echo "exec cat" > ap-cat.sh
+# chmod a+x ap-cat.sh
+# export DISPLAY=1
+# echo $PASSPHRASE | SSH_ASKPASS=./ap-cat.sh ssh-add deployment_key
+# rm ap-cat.sh
+git remote add deploy git@45.56.70.141:/~/Slack-CahBot/
 git push deploy
