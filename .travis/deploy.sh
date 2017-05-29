de09811f04ebe2c1609df9da05fbf008bba9141b
Jack Cai <ziyang_99@outlook.com>
2017-05-29 14:28:43 -0700

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,10 +5,5 @@ IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
-# echo "exec cat" > ap-cat.sh
-# chmod a+x ap-cat.sh
-# export DISPLAY=1
-# echo $PASSPHRASE | SSH_ASKPASS=./ap-cat.sh ssh-add deployment_key
-# rm ap-cat.sh
 git remote add deploy git@45.56.70.141:/~/slack-cahbot/Slack-CahBot/
 git push deploy
