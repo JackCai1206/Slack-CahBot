26c240f21c61e58c3c85e47865c9b9845edf976f
Jack Cai <ziyang_99@outlook.com>
2017-05-29 14:58:07 -0700

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,5 +5,6 @@ IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
-git remote add deploy git@45.56.70.141:/~/slack-cahbot/Slack-CahBot/
+git config --global push.default matching
+git remote add deploy git@45.56.70.141:/home/git/slack-cahbot/Slack-CahBot
 git push deploy
