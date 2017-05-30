6efdca4fea8eb56965dabead7b05aca161256b30
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-29 19:48:16 -0500

    pls

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,17 @@
-eval "$(ssh-agent -s)" #start the ssh agent
-chmod 600 .travis/id_rsa # this key should have push access
-ssh-add .travis/id_rsa
-IP=45.56.70.141
-if [ -z `ssh-keygen -F $IP` ]; then
-  ssh-keyscan -H $IP >> ~/.ssh/known_hosts
-fi
+# eval "$(ssh-agent -s)" #start the ssh agent
+# chmod 600 .travis/id_rsa # this key should have push access
+# ssh-add .travis/id_rsa
+# IP=45.56.70.141
+# if [ -z `ssh-keygen -F $IP` ]; then
+#   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
+# fi
 
-git config --global push.default matching
-git remote add deploy git@45.56.70.141:/home/git/slack-cahbot/Slack-CahBot/
-git push deploy master
+# git config --global push.default matching
+# git remote add deploy git@45.56.70.141:/home/git/slack-cahbot/Slack-CahBot/
+# git push deploy master
+
+git config user.email "ziyang_99@outlook.com"
+git config user.name "AsianPsychoBoy"
+
+git add .
+git commit -m "automated build"
--- .travis/deploy_build.sh
+++ .travis/deploy_build.sh
@@ -1,7 +1,1 @@
-git config user.email "ziyang_99@outlook.com"
-git config user.name "AsianPsychoBoy"
-
-git add .
-git commit -m "automated build"
-
 scp -r build git@45.56.70.141:~/slack-cahbot/
