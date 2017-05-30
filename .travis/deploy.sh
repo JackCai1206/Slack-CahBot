2df063bd43c551ff907c432ebef6af8e2e04c665
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-29 20:10:38 -0500

    last try

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
-# eval "$(ssh-agent -s)" #start the ssh agent
-# chmod 600 .travis/id_rsa # this key should have push access
-# ssh-add .travis/id_rsa
+eval "$(ssh-agent -s)" #start the ssh agent
+chmod 600 .travis/id_rsa # this key should have push access
+ssh-add .travis/id_rsa
 IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
