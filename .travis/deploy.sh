691c749ab23d9414c2906b929452332a5df276f2
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-29 20:08:39 -0500

    im really tired

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,10 +1,10 @@
 # eval "$(ssh-agent -s)" #start the ssh agent
 # chmod 600 .travis/id_rsa # this key should have push access
 # ssh-add .travis/id_rsa
-# IP=45.56.70.141
-# if [ -z `ssh-keygen -F $IP` ]; then
-#   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
-# fi
+IP=45.56.70.141
+if [ -z `ssh-keygen -F $IP` ]; then
+  ssh-keyscan -H $IP >> ~/.ssh/known_hosts
+fi
 
 # git config --global push.default matching
 # git remote add deploy git@45.56.70.141:/home/git/slack-cahbot/Slack-CahBot/
