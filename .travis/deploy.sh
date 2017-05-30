2a9d661663118635dc57cfcf419bdd7a16b1d7bf
AsianPsychoBoy <ziyang_99@outlook.com>
2017-05-29 19:22:12 -0500

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,6 +5,9 @@ IP=45.56.70.141
 if [ -z `ssh-keygen -F $IP` ]; then
   ssh-keyscan -H $IP >> ~/.ssh/known_hosts
 fi
+
+git add .
+git commit -m automated build
 git config --global push.default matching
 git remote add deploy git@45.56.70.141:/home/git/slack-cahbot/Slack-CahBot/
 git push deploy master
